c9eed2037ad77c7385ec128c90c97c5ebb767c09
Fake4091 <aswends7517@gmail.com>
2024-10-16 14:25:07 -0500

    I forgot to set up a repo before I started working... again

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.http.request import HttpRequest
+
+# Create your views here.
+
+
+def hey_you_view(request: HttpRequest, name) -> HttpResponse:
+    return HttpResponse(f"Hey {name.title()}!")
+
+
+def how_old_view(request: HttpRequest, end, birthyear) -> HttpResponse:
+    return HttpResponse({end - birthyear})
+
+
+def order_view(request: HttpRequest, burgers, fries, drinks) -> HttpResponse:
+    return HttpResponse("{:.2f}".format(((burgers * 4.5) + (fries * 1.5) + drinks)))
